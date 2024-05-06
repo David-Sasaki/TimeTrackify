17863710ed6c6f1ded61f569134255ba70a4323d
David-AI-Expert <davidsasaki1986@gmail.com>
2024-05-06 12:20:04 -0700

    Add ProjectList

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ProjectList from "./components/ProjectList/ProjectList";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <ProjectList />
+    </div>
+  );
+};
+
+export default App;
